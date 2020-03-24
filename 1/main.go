00c21d193bda90c0d0fc98ee01baebbb3b00d91b
Ado Kukic <kukicadnan@gmail.com>
2020-03-23 22:19:00 -0700

    Problem #1

---
+++ 1/main.go
@@ -0,0 +1,30 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	number := 1000
+	sum := 0
+
+	for i := 0; i < number; i++ {
+		result := isMultipleOfThreeOrFive(i)
+		if result {
+			sum += i
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func isMultipleOfThreeOrFive(number int) bool {
+	if number%3 == 0 || number%5 == 0 {
+		return true
+	}
+	return false
+}
